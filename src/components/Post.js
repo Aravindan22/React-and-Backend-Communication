d107aa4ebd710004d0f96d2ca27fa46f45d20dfd
Aravindan22 <aravindan7822@gmail.com>
2021-04-10 15:19:20 +0000

    Added Post Method

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-// import Axios from "axios";
-// import axios from "axios";
-// Axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-// Axios.defaults.headers.common["Content-type"] =
-//   "application/x-www-form-urlencoded";
+import Axios from "axios";
+var qs = require('qs');
 function Post() {
   const url = "https://woolly-joyous-pisces.glitch.me/api/test1";
   const [data, setData] = useState({
@@ -12,41 +9,27 @@ function Post() {
 
   async function submit(e) {
     e.preventDefault();
-    let res = await fetch(url, {
-      method: "post",
-      // mode: "no-cors",
-      headers: {
-        Accept: "*/*",
-        "Content-type": "application/x-www-form-urlencoded",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify({
-        key_test: "kkkkkk"
-      })
+    var body = qs.stringify({
+    'Name': data.name 
     });
-    console.log(res.body);
+    var config = {
+    method: 'post',
+    url: url,
+    headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : body
+    };
 
-    // Axios({
-    //   method: "post",
-    //   url: url,
-    //   data: {
-    //     ket: "jkkk"
-    //   }
-    // })
-    //   .then((data) => console.log(data))
-    //   .catch((e) => console.log(e));
-
-    // Axios.post(url, {
-    //   Name: data.name
-    // })
-    //   .then((res) => {
-    //     console.log(res);
-    //     // console.log(res.data);
-    //   })
-    //   .catch((e) => {
-    //     console.log(e);
-    //   });
-    // console.log(JSON.stringify(data));
+    Axios(config)
+    .then(function (response) {
+        let res =JSON.stringify(response.data) 
+    console.log(res);
+    window.alert(res);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
   }
 
   function handle(e) {
